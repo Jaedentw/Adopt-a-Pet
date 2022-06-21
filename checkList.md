ddd015f313418c6e95c5f5c33f14cebb6ab51422
Konstantin <kostakv@outlook.com>
2022-06-21 22:45:48 +0000

    added post/get to each EJS, added login/register functionality

---
+++ checkList.md
@@ -0,0 +1,76 @@
+### Pages
+[] Profile page for users
+    - name 
+    - profile image
+    - desc 
+    - posts 
+    - contact info
+
+
+[] Main Page for featured
+    - featured items****
+    - recommended/suggested
+
+
+[] Login
+    - username
+    - password
+
+[] register
+    - username
+    - password 2 entires
+    - email
+    - phone number
+
+[] saved items page
+    - list bookmarked items
+    - unavailble items
+    - favourites
+
+[] messages between users
+    - text box
+    - submission
+    - select user
+    - notification settings
+
+[] sales/post page (your listings)
+    - recent sales
+    - posted sales
+    - notifications on specific item
+
+[] settings page (users)
+    - updates
+    - notification
+    - change profile info (name, email, phonenumber, image)
+    - security - toggle phone number
+               - toggle email
+               - specific user visibility
+
+
+### functions
+
+
+
+### HTML / CSS 
+# HTML pages
+[] profile
+[] main
+[] settings
+[] messages
+[] login
+[] register
+[] saved items page
+[] sales post page
+
+# CSS pages
+[] profile
+[] main
+[] settings
+[] messages
+[] login
+[] register
+[] saved items page
+[] sales post page
+
+
+
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require('cookie-session')
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -21,6 +22,15 @@ app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['ABC/@432cuas42/as'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
 
 app.use(
   "/styles",
@@ -48,10 +58,151 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+// get request for home page
+
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "test@yahoo.ca",
+    username: "testUser",
+    password: "password"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "test123@gmail.ca",
+    username: "userTest",
+    password: "password123"
+  },
+}
+
+
+
+const checkUsername = function(username, password){
+  for (const key in users) {
+    if (username === users[key].username){
+      if (password === users[key].password){
+        return true;
+      }
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+const checkUserEmail = function (username, email){
+    for (const key in users) {
+      if (username === users[key].username || email === users[key].email){
+        return true;
+      }
+    }
+    return false;
+}
+
+
+
+
+
+
 app.get("/", (req, res) => {
-  res.render("index");
+  const templateVars = {};
+  res.render("index",templateVars);
 });
+
+
+// get request for login
+app.get("/login", (req, res) => {
+  const templateVars = {};
+  console.log("at the log in page")
+  res.render("login",templateVars);
+});
+
+// post request for login
+app.post("/login", (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+  if (checkUsername(username,password)){
+    res.redirect("/");
+    console.log(`Login successful!!!!!`)
+    console.log(users)
+  }
+  else {
+    res.redirect("login");
+    console.log(users)
+    console.log("Login failed!!!!!!")
+  }
+
+
+});
+
+// get request for register
+app.get("/register", (req, res) => {
+  const templateVars = {};
+  res.render("register",templateVars);
+});
+
+// post request for register
+app.post("/register", (req, res) => {
+
+  if (!req.body.email || !req.body.password || !req.body.username) {
+    res.status(400).send("Error, Please enter all fields requierd");
+
+  } else if (checkUserEmail(req.body.username,req.body.email)) {
+
+    res.status(400).send("Email or username already registered");
+
+  } else {
+    let userID = "hello";
+    users[userID] = {
+      userID,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    };
+  }
+  console.log(users)
+  res.redirect("/");
+});
+
+// get for profile page
+app.get("/profile", (req, res) => {
+  const templateVars = {};
+  res.render("profile",templateVars);
+});
+
+// post request for profile page
+app.post("/profile", (req, res) => {
+  res.render("register");
+});
+
+// get request for settings page
+app.get("/settings", (req, res) => {
+  const templateVars = {};
+  res.render("settings",templateVars);
+});
+
+// post request for settings page
+app.post("/settings", (req, res) => {
+  res.render("settings");
+});
+
+
+// Post to logout
+app.post("/logout", (req, res) => {
+  req.session.user_id = undefined; // Clears cookies, redirects to homepage.
+  res.redirect("/")
+
+});
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+
