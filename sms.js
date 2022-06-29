97de63aa0cee0a282c77b73555f61cc43c0b978a
Estuardo <estuardo_101@hotmail.com>
2022-06-29 15:22:07 -0400

    merge

---
+++ sms.js
@@ -0,0 +1,17 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+console.log(accountSid,authToken);
+const client = require('twilio')(accountSid, authToken);
+
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+18597626484',
+     to: '+1 15872247055'
+   })
+  .then(message => console.log(message.sid));
